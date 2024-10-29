120be12e1a7e4dd4acdd872014a1b47ae2448b73
Nightdawg <unqdawg@gmail.com>
2024-10-30 00:09:41 +0200

    Add Heap Dump arguments to Play.bat

--- Play.bat
+++ Play.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk-15.0.2\bin\java.exe" -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m -jar hafen.jar
+"C:\Program Files\Java\jdk-15.0.2\bin\java.exe" -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="heapdumps" -jar hafen.jar
