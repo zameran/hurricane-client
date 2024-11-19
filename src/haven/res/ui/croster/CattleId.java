55d7978991e576d30fb634a10914d76ff9b22bbb
Nightdawg <unqdawg@gmail.com>
2024-11-19 22:10:04 +0200

    Improve Cattle Roster Names and Selection Mark

--- src/haven/res/ui/croster/CattleId.java
+++ src/haven/res/ui/croster/CattleId.java
@@ -9,9 +9,14 @@ import haven.MenuGrid.Pagina;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+import static haven.PUtils.blurmask2;
+import static haven.PUtils.rasterimg;
+
 @haven.FromResource(name = "ui/croster", version = 75)
 public class CattleId extends GAttrib implements RenderTree.Node, PView.Render2D {
     public final UID id;
+	public static final Text.Foundry fnd = new Text.Foundry(Text.dfont, 12);
+	public static final Tex selectionMark = Resource.loadtex("customclient/cattleroster/selectionMark");
 
     public CattleId(Gob gob, UID id) {
 	super(gob);
@@ -54,22 +59,22 @@ public class CattleId extends GAttrib implements RenderTree.Node, PView.Render2D
     private int lgrp;
     private Tex rnm;
     public void draw(GOut g, Pipe state) {
-	Coord sc = Homo3D.obj2view(new Coord3f(0, 0, 25), state, Area.sized(g.sz())).round2();
+	Coord sc = Homo3D.obj2view(new Coord3f(0, 0, 15), state, Area.sized(g.sz())).round2();
 	if(sc.isect(Coord.z, g.sz())) {
 	    Entry entry = entry();
 	    int grp = (entry != null) ? entry.grp : 0;
 	    String name = (entry != null) ? entry.name : null;
 	    if((name != null) && ((rnm == null) || !name.equals(lnm) || (grp != lgrp))) {
 		Color col = BuddyWnd.gc[grp];
-		rnm = new TexI(Utils.outline2(Text.render(name, col).img, Utils.contrast(col)));
+		rnm = new TexI(rasterimg(blurmask2(Utils.outline2(fnd.render(name, col).img, Color.BLACK, true).getRaster(), UI.rscale(1.0), UI.rscale(1.0), Color.BLACK)));
 		lnm = name;
 		lgrp = grp;
 	    }
 	    if((rnm != null) && (wnd != null) && wnd.visible) {
-		Coord nmc = sc.sub(rnm.sz().x / 2, -rnm.sz().y);
-		g.image(rnm, nmc);
+		Coord nmc = sc.sub(rnm.sz().x / 2, 0);
+		g.aimage(rnm, sc, 0.5, 1.0);
 		if((entry != null) && entry.mark.a)
-		    g.image(CheckBox.smark, nmc.sub(CheckBox.smark.sz().x, 0));
+		    g.aimage(selectionMark, nmc.sub(selectionMark.sz().x, 0), 0.1, 1.3);
 	    }
 	}
     }
