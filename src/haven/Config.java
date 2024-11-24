6282ec20b8ad5511c4e8491c5c7d54200f9db0bb
Nightdawg <unqdawg@gmail.com>
2024-11-24 23:52:14 +0200

    v1.14a

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -41,7 +41,7 @@ public class Config {
     public static final String confid = "Hurricane";
     public static final Variable<Boolean> par = Variable.def(() -> true);
     public final Properties localprops = getlocalprops();
-	public static final String clientVersion = "v1.14";
+	public static final String clientVersion = "v1.14a";
 	public static String githubLatestVersion = "Loading...";
 
     private static Config global = null;
