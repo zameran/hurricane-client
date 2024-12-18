b671cc92c1d0efe45b936a6a2aece2f63d325482
Nightdawg <unqdawg@gmail.com>
2024-12-18 02:51:39 +0200

    Make Propeller Cap 500% cooler

--- src/haven/SkelSprite.java
+++ src/haven/SkelSprite.java
@@ -264,6 +264,10 @@ public class SkelSprite extends Sprite implements Sprite.CUpd, EquipTarget, Spri
     }
     
     public void added(RenderTree.Slot slot) {
+	if (this.res.name.equals("gfx/terobjs/items/hats/propellercap")) {
+		slot.cstate(Pipe.Op.compose(Location.rot(new Coord3f(0, 0, 1), 3.15f), Location.rot(new Coord3f(0, 1, 0), -0.2f),
+				Location.scale(1.05f, 1.05f, 1.2f), Location.xlate(new Coord3f(0, 0, -0.5f))));
+	}
 	parts(slot);
 	slots.add(slot);
     }
