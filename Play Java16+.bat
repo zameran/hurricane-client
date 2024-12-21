bd44df64aae4d9706455b0c82e960668280812dc
Nightdawg <unqdawg@gmail.com>
2024-12-21 06:43:49 +0200

    Remove Java 16+ launcher, and make Play.bat just use the latest installed java

--- Play Java16+.bat
+++
@@ -1,1 +0,0 @@
-java -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="heapdumps" -jar hafen.jar
